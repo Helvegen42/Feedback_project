369f06a37a210e9d915e87615968c91156b50e54
IlyaLevitskiy <levitskiyilya42@gmail.com>
2022-07-19 16:47:00 +0300

    first commit

---
+++ feedback-app/src/App.js
@@ -0,0 +1,15 @@
+import Header from "./components/Header";
+import FeedbackItem from "./components/FeedbackItem";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <div className="container">
+        <FeedbackItem />
+      </div>
+    </>
+  );
+};
+
+export default App;
